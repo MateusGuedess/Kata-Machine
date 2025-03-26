a0b37d259c66472558f3a6a6f43c5601610a1bd9
Mateus Guedes <mxteusgued@gmail.com>
2025-03-26 00:58:32 -0300

    two balls crystal

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,18 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+    let j = 0;
+    let i = 0;
+    for (i; i < breaks.length; i = i + Math.sqrt(breaks.length)) {
+        if (breaks[i] === true) {
+            break;
+        }
+        j = i;
+    }
 
+    for (j; j < i; j++) {
+        if (breaks[j] === true) {
+            return j;
+        }
+    }
+
+    return -1;
 }
