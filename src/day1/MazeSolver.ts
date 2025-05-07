e89fb29bff56fce32b344b6d2c3038b0e1e5245e
Mateus Guedes <mxteusgued@gmail.com>
2025-05-07 02:31:29 -0300

    recursion: Path finding - base case

--- src/day1/MazeSolver.ts
+++ src/day1/MazeSolver.ts
@@ -1,3 +1,35 @@
-export default function solve(maze: string[], wall: string, start: Point, end: Point): Point[] {
+function walk(
+    maze: string[],
+    wall: string,
+    curr: Point,
+    end: Point,
+    seen: boolean[][],
+): boolean {
+    // 1. Base Case
+    // off the map
+    if (curr.x < 0 || curr.x >= maze[0].length || curr.y < 0 || maze.length) {
+        return false;
+    }
 
+    // on a wall
+    if (maze[curr.y][curr.x] === wall) {
+        return false;
+    }
+
+    // got where we want to go
+    if (curr.x === end.x && curr.y === end.y) {
+        return true;
+    }
+
+    // already seen
+    if (seen[curr.y][curr.x]) {
+        return false;
+    }
 }
+
+export default function solve(
+    maze: string[],
+    wall: string,
+    start: Point,
+    end: Point,
+): Point[] {}
